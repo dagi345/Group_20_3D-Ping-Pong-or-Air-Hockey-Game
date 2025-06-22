00213084f30f565254696d5d35d5d6f29ea76dbf
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-06-22 08:30:23 +0300

    add and Update physics.js

--- js/physics.js
+++ js/physics.js
@@ -1,3 +1,26 @@
 // physics.js
+// js/physics.js
+let world;
+let puckMaterial, paddleMaterial, wallMaterial;
 
-m
+export function initCannon() {
+    world = new CANNON.World();
+    world.gravity.set(0, -9.82, 0);
+    world.broadphase = new CANNON.SAPBroadphase(world);
+    world.allowSleep = true;
+    world.solver.iterations = 20; 
+
+    puckMaterial = new CANNON.Material('puckMaterial');
+    paddleMaterial = new CANNON.Material('paddleMaterial');
+    wallMaterial = new CANNON.Material('wallMaterial');
+    
+    return { world, puckMaterial, paddleMaterial, wallMaterial };
+}
+
+export function setupContactMaterials(_world, _puckMaterial, _paddleMaterial, _wallMaterial) {
+    _world.addContactMaterial(new CANNON.ContactMaterial(_puckMaterial, _wallMaterial, { friction: 0.025, restitution: 0.92 }));
+    _world.addContactMaterial(new CANNON.ContactMaterial(_puckMaterial, _paddleMaterial, { friction: 0.025, restitution: 0.98 }));
+    _world.addContactMaterial(new CANNON.ContactMaterial(_paddleMaterial, _wallMaterial, { friction: 0.06, restitution: 0.15 }));
+}
+
+
